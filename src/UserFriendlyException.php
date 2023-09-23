c3cb8edc84dbc49c3ac8d35dc7f0626c46723af5
Christian Brown <54856120+christianjbrown@users.noreply.github.com>
2023-09-23 18:22:36 +0100

    Initial commit

---
+++ src/UserFriendlyException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ChristianBrown\UserFriendlyException;
+
+use RuntimeException;
+
+final class UserFriendlyException extends RuntimeException implements UserFriendlyExceptionInterface
+{
+}
